099d7d9cb3adc6943e6d094a94b0af3c494e7d5e
Chukwunalu <naluprosper54@gmail.com>
2022-09-26 12:33:27 +0100

    second commit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,105 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+User = get_user_model()
+
+CATEGORY_CHOICES = (
+    ('s', 'Shirt '),
+    ('sw', 'Sport wear'),
+    ('ow', 'Outwear')
+)
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    description = models.TextField()
+      
+    def __str__(self):
+        return self.title
+    
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='image', on_delete=models.CASCADE)
+    image = models.ImageField()
+        
+
+class OrderedProduct(models.Model):
+    user = models.ForeignKey(User, 
+                             on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+ 
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
+    
+    def get_total_item_price(self):
+        return self.quantity * self.product.price
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, 
+                             on_delete=models.CASCADE)
+    products = models.ManyToManyField(OrderedProduct)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    shipping_address = models.ForeignKey('Address', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
+    payment = models.ForeignKey('Payment', on_delete=models.SET_NULL, blank=True, null=True)
+    being_delivered = models.BooleanField(default=False)
+    received = models.BooleanField(default=False)
+    refund_requested = models.BooleanField(default=False)
+    refund_granted = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user.username
+    
+    def get_total(self):
+        total = 0
+        for order_product in self.products.all():
+            total += order_product.get_final_price()
+        
+        if self.coupon:
+            total -= self.coupon.amount          
+        return total
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, 
+                             on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    country = CountryField(multiple=False)
+    zip = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name_plural = 'Addresses'
+    
+    
+class Payment(models.Model):
+    paypal_id = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username    
+    
+class Refund(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    reason = models.TextField()
+    accepted = models.BooleanField(default=False)
+    email = models.EmailField()
+    
+    def __str__(self) -> str:
+        return f"{self.pk}"
+    
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ecommerce',
+    
+    'django_countries'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +81,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -116,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
