02ec0dbaf53a80d17d06ee5fb851909e0857e42b
nahloo-obi <naluprosper54@gmail.com>
2023-12-13 16:07:32 +0000

    docker update

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,13 +3,13 @@
 set -e
 
 # Create necessary directories
-mkdir -p /app/staticfiles
-mkdir -p /static
-mkdir -p /media
-mkdir -p /vol
+# mkdir -p /app/staticfiles
+# mkdir -p /static
+# mkdir -p /media
+# mkdir -p /vol
 
-# Set correct permissions
-chown -R app:app /app/staticfiles /static /media /vol
+# # Set correct permissions
+# chown -R app:app /app/staticfiles /static /media /vol
 
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
