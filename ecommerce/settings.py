9634f87e8e1d0d3e0f99da902a9d2bd30761d1c7
nahloo-obi <naluprosper54@gmail.com>
2023-12-16 13:40:32 +0000

    v

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 # MEDIA_URL = 'static/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -164,6 +164,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
