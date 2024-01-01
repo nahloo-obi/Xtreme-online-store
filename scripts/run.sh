fbac5f74b7677831b55aa9c76f7f0a894cdca0a3
nahloo-obi <naluprosper54@gmail.com>
2024-01-01 17:02:47 +0000

    test

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,6 +8,4 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 echo "Migrations has been done"
-python manage.py create_super_user
-
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module ecommerce.wsgi
